c64a484b466b0d7b7a7f6f10abec802cfb6f1950
Xu Lin <shawnlx0701@hotmail.com>
2020-02-09 21:21:55 -0500

    Added comments and improved variable names

--- prime.py
+++ prime.py
@@ -2,18 +2,22 @@
 prime.py -- Write the application code here
 """
 def generate_prime_factors(Number):
+    #setting the variables and list
     count = 0
-    intergers = []
+    prime_factors = []
     dividend = Number
+
+    #for loop to check for prime factors and append them to a list
     for i in range(2, (Number//2+1)):
         while dividend % i == 0:
-            intergers.append(i)
+            prime_factors.append(i)
             dividend = dividend//i
             count = count + 1
             i = 2
 
+    # returning the result and the list of prime factors
     if count == 0 and Number != 1:
         print(" %d is a Prime Number" %Number)
     else:
         print(" %d is not a Prime Number" %Number)
-        print(intergers)
+        print(prime_factors)
